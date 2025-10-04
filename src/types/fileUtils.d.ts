3b354afd58e7d92fda427619fc150405c47eff2c
troner14 <73949396+troner14@users.noreply.github.com>
2025-10-04 19:47:07 +0200

    feat: agregar nuevas rutas de servicios al tipo AllFolders en fileUtils

--- src/types/fileUtils.d.ts
+++ src/types/fileUtils.d.ts
@@ -1,2 +1,2 @@
 // fichero generado automaticamente NO modificar //
-export type AllFolders = 'class' | 'class/loaders' | 'commands' | 'commands/basic' | 'components' | 'events' | 'types' | 'utils';
+export type AllFolders = 'class' | 'class/loaders' | 'commands' | 'commands/basic' | 'commands/virtualization' | 'components' | 'events' | 'services' | 'services/virtualization' | 'services/virtualization/providers' | 'types' | 'utils';
